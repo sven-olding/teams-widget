cae73bd40c5d757502672e89a49ca374839d3f5a
Sven Olding <sven.olding@gmx.de>
2021-01-29 16:17:28 +0100

    fix test button for ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 
 function App(): JSX.Element {
     const [counter, setCounter] = useState(0);
-
+    if (counter == 1) {
+        throw new Error('Test');
+    }
     return (
-        <div>
-            <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
+        <div className="lg:container lg:mx-auto m-4">
+            <div className="p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
                 <div className="ml-6 pt-1">
                     <h1 className="text-2xl text-blue-700 leading-tight">
                         Tailwind and Create React App
@@ -13,12 +15,12 @@ function App(): JSX.Element {
                     <p className="text-base text-gray-700 leading-normal">{counter}</p>
                 </div>
             </div>
+
             <button
                 type="button"
                 className="border border-red-500 text-red-500 hover:bg-red-500 hover:text-gray-200 rounded px-4 py-2 max-w-md flex mx-auto"
                 onClick={() => {
                     setCounter(counter + 1);
-                    throw new Error('Test');
                 }}>
                 Fehler provozieren
             </button>
