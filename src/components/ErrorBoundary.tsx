9efdf632490f7d7e9d34ca7ec8e5ea32c9b8b30b
Sven Olding <sven.olding@gmx.de>
2021-01-29 15:24:52 +0100

    add error boundary at top level

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+interface ErrorBoundaryProps {
+    children: JSX.Element;
+}
+
+interface ErrorBoundaryState {
+    error?: Error;
+    errorInfo?: React.ErrorInfo;
+    hasError: boolean;
+}
+
+export class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+    constructor(props: ErrorBoundaryProps) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+        return { hasError: true, error: error };
+    }
+
+    componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+        console.error(error);
+        this.setState({ error, errorInfo });
+    }
+
+    render(): JSX.Element {
+        const { hasError, error } = this.state;
+        if (hasError) {
+            return (
+                <div>
+                    <div>
+                        <p>
+                            Beim Laden der Seite ist ein Fehler aufgetreten{' '}
+                            <span
+                                style={{ cursor: 'pointer', color: '#0077FF' }}
+                                onClick={() => {
+                                    window.location.reload();
+                                }}
+                                onKeyDown={() => {
+                                    window.location.reload();
+                                }}
+                                role="button"
+                                tabIndex={0}>
+                                Diese Seite neu laden
+                            </span>{' '}
+                        </p>
+                    </div>
+                    <div>
+                        <details style={{ whiteSpace: 'pre-wrap' }}>
+                            <summary>Details</summary>
+                            {error?.stack}
+                        </details>
+                    </div>
+                </div>
+            );
+        } else {
+            return this.props.children;
+        }
+    }
+}
