9f0b2a1aaaf24a01391cf5df01f5d05ceeb9636d
Sven Olding <sven.olding@gmx.de>
2021-01-29 16:19:48 +0100

    remove errorboundary testing code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 function App(): JSX.Element {
-    const [counter, setCounter] = useState(0);
-    if (counter == 1) {
-        throw new Error('Test');
-    }
     return (
         <div className="lg:container lg:mx-auto m-4">
             <div className="p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
@@ -12,18 +8,9 @@ function App(): JSX.Element {
                     <h1 className="text-2xl text-blue-700 leading-tight">
                         Tailwind and Create React App
                     </h1>
-                    <p className="text-base text-gray-700 leading-normal">{counter}</p>
+                    <p className="text-base text-gray-700 leading-normal">Test</p>
                 </div>
             </div>
-
-            <button
-                type="button"
-                className="border border-red-500 text-red-500 hover:bg-red-500 hover:text-gray-200 rounded px-4 py-2 max-w-md flex mx-auto"
-                onClick={() => {
-                    setCounter(counter + 1);
-                }}>
-                Fehler provozieren
-            </button>
         </div>
     );
 }
