8b1c6318ad0046a190ee828fcc4d12a20c5b3012
Sven Olding <sven.olding@gmx.de>
2021-01-29 08:46:58 +0100

    add eslint prettier husky and eslint-staged

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    env: {
+        browser: true,
+        amd: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:jsx-a11y/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+    ],
+    rules: {
+        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    },
+    plugins: ['simple-import-sort'],
+};
