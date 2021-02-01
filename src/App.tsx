82d954711810aa564a0a526a3dbfa262cbe745eb
Sven Olding <sven.olding@gmx.de>
2021-02-01 12:05:48 +0100

    add first call to new graph client

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useGraphClient } from './hooks/msgraph-client';
+import { User } from '@microsoft/microsoft-graph-types';
 
 function App(): JSX.Element {
+    const [userDisplayName, setUserDisplayName] = useState('');
+    const graphClient = useGraphClient();
+
+    useEffect(() => {
+        async function getUserDisplayName() {
+            try {
+                const user: User = await graphClient.api('/me').get();
+                console.log(user);
+                setUserDisplayName(user.displayName ? user.displayName : '');
+            } catch (e) {
+                console.error(e);
+            }
+        }
+        getUserDisplayName();
+    }, [graphClient, userDisplayName]);
+
     return (
         <div className="lg:container lg:mx-auto m-4">
             <div className="p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
                 <div className="ml-6 pt-1">
-                    <h1 className="text-2xl text-blue-700 leading-tight">
-                        Tailwind and Create React App
-                    </h1>
-                    <p className="text-base text-gray-700 leading-normal">Test</p>
+                    <h1 className="text-2xl text-blue-700 leading-tight">MS Teams Widget</h1>
+                    {userDisplayName && (
+                        <p className="text-base text-gray-700 leading-normal">
+                            Willkommen, {userDisplayName}
+                        </p>
+                    )}
                 </div>
             </div>
         </div>
